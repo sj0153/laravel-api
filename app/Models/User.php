6e4358acbaab58fe0265806a9b9e680c30abf3f3
sj0153 <fiste.ooo@gmail.com>
2022-07-11 21:42:33 +0200

    v 1.5
    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,16 @@ class User extends Authenticatable
         
     use HasApiTokens, HasFactory, Notifiable;
 
-  
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
     /**
      * The attributes that should be hidden for serialization.
